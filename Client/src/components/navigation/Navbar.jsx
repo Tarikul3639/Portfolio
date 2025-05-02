80918a35fe8f1cb6a7e02806acee120cfa222b56
Tarikul Islam <123243130+Tarikul3639@users.noreply.github.com>
2025-05-03 01:13:13 +0600

    NavBar color fixed

--- Client/src/components/navigation/Navbar.jsx
+++ Client/src/components/navigation/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = ({ title = "Tarikul", menuItems = ["HOME", "ABOUT", "PROJECTS", "
     };
 
     return (
-        <nav className={`${isOpen ? "bg-background/95 dark:bg-background-dark/95" : " bg-transparent backdrop-blur-xs"} w-full min-w-[250px] text-white px-4 py-0 shadow-xl fixed top-0 left-0 z-50`}>
+        <nav className={`${isOpen ? "bg-background-light/95 dark:bg-background-dark/95" : " bg-transparent backdrop-blur-xs"} w-full min-w-[250px] text-white px-4 py-0 shadow-xl fixed top-0 left-0 z-999`}>
             <div className="container mx-auto flex items-center justify-between">
                 {/* Logo */}
                 <Link className="text-primary-light dark:text-primary-dark z-10 cursor-pointer overflow-hidden" to="home" smooth={true} duration={500}>
@@ -60,11 +60,14 @@ const Navbar = ({ title = "Tarikul", menuItems = ["HOME", "ABOUT", "PROJECTS", "
                     className="md:hidden text-bold text-white cursor-pointer transition-all duration-500 ease-in-out focus:outline-none z-10"
                     onClick={() => setIsOpen(!isOpen)}
                 >
-                    {isOpen ? <FontAwesomeIcon icon={faXmark} className="text-3xl text-primary" /> : <HiOutlineMenuAlt3 size={35} color="#cbcbcb" />}
+                    {isOpen ? <FontAwesomeIcon icon={faXmark} className="text-3xl text-primary-light opacity-80 dark:text-primary-dark" /> : <span className="text-primary-light opacity-80 dark:text-primary-dark">
+                        <HiOutlineMenuAlt3 size={35} />
+                    </span>
+                    }
                 </button>
                 {/* Navigation Menu with Slide Animation */}
-                <ul className={`Menu flex flex-col items-center text-left md:flex-row md:space-x-10 space-y-2 md:space-y-1 pb-6 md:p-0 absolute md:static md:bg-transparent left-0 w-full md:w-auto transition-all duration-300 ease-in-out 
-                    ${isOpen ? "top-12 opacity-100 bg-background/95 dark:bg-background-dark/95 shadow-lg md:shadow-none" : "-top-60 opacity-100 h-auto"}`}>
+                <ul className={`Menu flex flex-col items-center text-left md:flex-row md:space-x-10 space-y-2 md:space-y-1 pb-6 md:p-0 absolute md:static md:bg-transparent left-0 w-full md:w-auto transition-all duration-100 ease-in-out 
+                    ${isOpen ? "top-12 opacity-100 bg-background-light/95 dark:bg-background-dark/95 shadow-lg md:shadow-none" : "-top-60 opacity-100 h-auto"}`}>
 
                     {/* Theme Toggle */}
                     <button onClick={toggleTheme} className="flex item-center text-xl text-primary-light dark:text-primary/60 hover:text-primary-light/60 dark:hover:text-primary/90 transition py-2 px-3 transition-all duration-300 cursor-pointer rounded-lg">
